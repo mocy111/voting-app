7876c172d292eee89ce287ef1651e4281a79073c
MOCTAR YONLI <yonli.moctar@codeloccol.org>
2020-10-22 17:28:09 +0100

    Updates

---
+++ precache-manifest.b46224c903adbcc9e7a7403649f35d74.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58864a3e8890ff432e295a3500875556",
+    "url": "/voting-app/index.html"
+  },
+  {
+    "revision": "933b3c6731fa912c8fdf",
+    "url": "/voting-app/static/css/main.f799fb5e.chunk.css"
+  },
+  {
+    "revision": "4ead53aa878dbd1f7ce4",
+    "url": "/voting-app/static/js/2.87dcd3c4.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/voting-app/static/js/2.87dcd3c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "933b3c6731fa912c8fdf",
+    "url": "/voting-app/static/js/main.6530ac28.chunk.js"
+  },
+  {
+    "revision": "10b014b2dfb8afe8325a",
+    "url": "/voting-app/static/js/runtime-main.5935a110.js"
+  },
+  {
+    "revision": "1630c2a6dc314a29c8ddee51d707dd26",
+    "url": "/voting-app/static/media/az.1630c2a6.jpg"
+  },
+  {
+    "revision": "ad8b1fef30436478df87715de845c08e",
+    "url": "/voting-app/static/media/houssein.ad8b1fef.jpg"
+  },
+  {
+    "revision": "0e8e07d524c597987b08f86e9e22fdc7",
+    "url": "/voting-app/static/media/imran.0e8e07d5.jpg"
+  },
+  {
+    "revision": "829a4d37cb027c074a1f1c289b6d0448",
+    "url": "/voting-app/static/media/youss.829a4d37.jpg"
+  }
+]);
